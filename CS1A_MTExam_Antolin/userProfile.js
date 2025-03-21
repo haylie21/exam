d563e82d3077986edfcbbb4e5f3dd413addb53c6
haylie21 <haylie.antolin08@gmail.com>
2025-03-21 22:31:41 +0800

    Create userProfile.js

---
+++ CS1A_MTExam_Antolin/userProfile.js
@@ -0,0 +1,12 @@
+// Collect basic user information and store it in variables  
+let fullName = prompt("Enter your full name:");  
+let age = prompt("Enter your age:");  
+let favoriteNumber = prompt("Enter your favorite number:");  
+let favoriteColor = prompt("Enter your favorite color:");  
+
+// Display the collected information in the console  
+console.log("User Information:");  
+console.log("Full Name:", fullName);  
+console.log("Age:", age);  
+console.log("Favorite Number:", favoriteNumber);  
+console.log("Favorite Color:", favoriteColor);
