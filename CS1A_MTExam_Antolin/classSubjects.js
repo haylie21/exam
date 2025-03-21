fce0d914146e1770411546e13d01de19fce23dab
haylie21 <haylie.antolin08@gmail.com>
2025-03-21 22:33:17 +0800

    Create classSubjects.js

---
+++ CS1A_MTExam_Antolin/classSubjects.js
@@ -0,0 +1,9 @@
+// Asking the user for class details and storing them in variables  
+let subjectTitle = prompt("Enter the subject title:");
+let classSchedule = prompt("Enter the class schedule (Time, Days):");
+let classroom = prompt("Enter the classroom:");
+let classInstructor = prompt("Enter the class instructor:");
+let studentName = prompt("Enter your name:");
+
+// Showing the collected info in the console  
+console.log(${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.);
